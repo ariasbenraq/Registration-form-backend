44b28c30e17bf0581bb57928ccb5ad739e123448
Renzo Arias <rariasbarrero@gmail.com>
2025-05-30 01:56:15 -0500

    Configuracion de entorno de produccion

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,26 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
+import * as dotenv from "dotenv";
+
+// Carga el archivo .env correspondiente
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors(); // permite peticiones desde React
+  // Permitir CORS desde cualquier origen o uno específico (mejor práctica)
+  app.enableCors({
+    origin: process.env.ALLOWED_ORIGIN || "*", // Por seguridad puedes limitarlo luego
+    credentials: true,
+  });
 
-  // 🛡️ Habilita la validación automática con DTOs
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(3000, '0.0.0.0');
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port, "0.0.0.0");
+
+  console.log(`✅ Backend corriendo en http://localhost:${port}`);
 }
 bootstrap();
